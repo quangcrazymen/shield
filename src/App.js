dc9c7376d53c004aff137aedbe1029998e8737be
Quang <quangcrazymen@gmail.com>
2022-06-02 14:48:30 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -31,11 +31,11 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      {/* <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/news'>News</Link></li>
-        <li><Link to='/contact'>Contact</Link></li>
-      </ul> */}
+      <ul>
+        {/* <li><Link to='/'>Home</Link></li> */}
+        {/* <li><Link to='/news'>News</Link></li> */}
+        <li><Link to='/welcome'>Welcome</Link></li>
+      </ul>
       <Routes>
         <Route path="/" element={ <HomePage/> }/>
         <Route path="/news" element={ <NewsPage/> }/>
