b6b5eb0a7688284ab72b71870e7d321cdcc54cd8
Quang <quangcrazymen@gmail.com>
2022-06-02 16:35:07 +0700

    figured it how to solve the routing problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import { Routes,Route,Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import HomePage from './pages/Home/index'
 import NewsPage from './pages/News/index'
 import WelcomePage from './pages/Welcome/index';
@@ -28,18 +28,24 @@ import './App.css';
 // }
 
 // export default App;
+
+// Animation: https://www.youtube.com/watch?v=FdrEjwymzdY
+// https://stackoverflow.com/questions/52480688/how-to-render-different-layouts-using-react
+
 function App() {
   return (
     <div className='App'>
-      <ul>
-        {/* <li><Link to='/'>Home</Link></li> */}
-        {/* <li><Link to='/news'>News</Link></li> */}
+      {/* <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/news'>News</Link></li>
         <li><Link to='/welcome'>Welcome</Link></li>
-      </ul>
+      </ul> */}
+      {/* <WelcomePage/> */}
+      {/* <HomePage /> */}
       <Routes>
-        <Route path="/" element={ <HomePage/> }/>
-        <Route path="/news" element={ <NewsPage/> }/>
-        <Route path="welcome" element={<WelcomePage/>}/>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/news" element={<WelcomePage/>} />
+        {/* <Route path="welcome" element={<WelcomePage />} /> */}
       </Routes>
     </div>
 
--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,9 +1,12 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import "../styles/info-card.css"
 function InfoCard(props) {
     return (
         <div className='info-card'>
-            <a href="/login">{props.name}</a>
+            {/* <a href="/home">{props.name}</a> */}
+            <Link className="link-tag" to="/home">{props.name}</Link>
         </div>
+        
     );
 }
 
