ad34285d052337c4738e8c971c57374e8b9b421b
Quang <quangcrazymen@gmail.com>
2022-06-05 10:33:31 +0700

    finish top nav bar of the home page

--- src/App.js
+++ src/App.js
@@ -6,29 +6,6 @@ import WelcomePage from './pages/Welcome/index';
 
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//             Hello world
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 // Animation: https://www.youtube.com/watch?v=FdrEjwymzdY
 // https://stackoverflow.com/questions/52480688/how-to-render-different-layouts-using-react
 
@@ -44,7 +21,7 @@ function App() {
       {/* <HomePage /> */}
       <Routes>
         <Route path="/home" element={<HomePage />} />
-        <Route path="/news" element={<WelcomePage/>} />
+        <Route path="/welcome" element={<WelcomePage/>} />
         {/* <Route path="welcome" element={<WelcomePage />} /> */}
       </Routes>
     </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,24 @@
+import sideBarBtn from "../../images/sideBarBtn.png"
+import logo from "../../images/smallLogo.png"
+import profilePicture from "../../images/profilePicture.png"
+import "./style.css"
 // Use htmltagwrap Alt+W
 function Home() {
-    return ( <h1>Home Page</h1> );
+    return (
+        <div className="homepage">
+            <div className="topNav">
+                <div className="topNav--left">
+                    <img className="sideBarBtn" src={sideBarBtn} alt="sideBarBtn" />
+                    <img className="logo--small" src={logo} alt="Small logo" />
+                    <h2 className="logo--text">Shield.</h2>
+                </div>
+                <img className="picture--profile" src={profilePicture} alt="Profile pic" />
+            </div>
+            <div className="content">
+                <h1>Content goes here</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
